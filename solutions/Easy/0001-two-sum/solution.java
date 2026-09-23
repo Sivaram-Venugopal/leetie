ef3d8ab6e2905597351a44f2bcad7cc54e60ee98
Sivaram-Venugopal <sivaramv304@gmail.com>
2026-09-23 14:56:02 +0000

    leetie: Add 1. Two Sum [Easy] (java)

--- solutions/Easy/0001-two-sum/solution.java
+++ solutions/Easy/0001-two-sum/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table
 // Link     : https://leetcode.com/problems/two-sum/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42804000 (beats 0%)
+// Runtime  : 44 ms (beats 34%)
+// Memory   : 47060000 (beats 59%)
 // Language : java
 // Copyright: (c) 2026 Sivaram-Venugopal. All rights reserved.
 // Synced by: leetie
